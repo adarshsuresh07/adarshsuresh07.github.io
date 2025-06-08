5c6d0436ca59a34c3d38d565106bc19a1f8d4e13
Adarsh S <89060786+adarshsuresh12@users.noreply.github.com>
2025-06-08 14:02:14 +0530

    chore: adding changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // For username.github.io (root domain)
   server: {
     port: 3000,
     open: true
